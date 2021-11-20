b865e11593cb00c15bb5490d655852ec18300c74
bwprado <bwprado@gmail.com>
2021-11-19 23:21:17 -0300

    update

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ class Bootstrap extends HTMLElement {
 		const scriptBody = document.createElement('script')
 		const body = document.querySelector('body')
 		const div = document.createElement('div')
-    
-    div.innerHTML = `
-    <div class="container">
-    <button type="button" class="btn btn-primary">Primary</button>
-  </div>
-    `
 
+		div.innerHTML = `
+    <div class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-primary">Left</button>
+  <button type="button" class="btn btn-primary">Middle</button>
+  <button type="button" class="btn btn-primary">Right</button>
+</div>
+    `
 		script.type = 'text/javascript'
 		script.src =
 			'https://cdn.jsdelivr.net/gh/bwprado/bootstrap@latest/script.js'
@@ -28,13 +29,9 @@ class Bootstrap extends HTMLElement {
 		scriptBody.integrity =
 			'sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p'
 
-		link.rel = 'stylesheet'
-		link.type = 'text/css'
-		link.integrity =
-			'sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3'
-		link.crossorigin = 'anonymous'
-		link.href =
-			'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
+		link.innerHTML = `
+			<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+		`
 
 		head.appendChild(link)
 		head.appendChild(script)
